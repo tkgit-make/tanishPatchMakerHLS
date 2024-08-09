52809db44ac3429da05887d0d7b9c65230676b5a
unknown <tanish.ohs@gmail.com>
2024-08-08 21:44:49 -0400

    Added large circuit rtl

--- PatchMaker_tanishGit/solutionLC/script.tcl
+++ PatchMaker_tanishGit/solutionLC/script.tcl
@@ -13,8 +13,9 @@ add_files -tb tanishTestBench/wedgeData_v3_128.txt -cflags "-Wno-unknown-pragmas
 open_solution "solutionLC" -flow_target vivado
 set_part {xcvu19p-fsvb3824-2-e}
 create_clock -period 3 -name default
+config_export -format ip_catalog -rtl verilog
 source "./PatchMaker_tanishGit/solutionLC/directives.tcl"
 csim_design
 csynth_design
 cosim_design
-export_design -rtl verilog -format ip_catalog
+export_design -rtl verilog -format ip_catalog -output C:/Users/rapiduser/Desktop/tanishGitHub/tanishPatchMakerHLS/VivadoRTL/MPSQ.zip
